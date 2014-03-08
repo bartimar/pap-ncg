1539f3b3f54a162dd7915e81752825f08b3156bd
Marek <bartimar6@gmail.com>
2014-03-08 15:23:41 +0100

    Dijkstra pro vsechny uzly

--- PAP/PAP.cpp
+++ PAP/PAP.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 int *dijkstraDistance (int vertices[NUMBERofVERTICES][NUMBERofVERTICES]);
 void findNearest (int s, int e, int minimumDistance[NUMBERofVERTICES], bool connected[NUMBERofVERTICES], int *d, int *v);
-void init (int vertices[NUMBERofVERTICES][NUMBERofVERTICES]);
+void init (int vertices[NUMBERofVERTICES][NUMBERofVERTICES],int=0);
 void updateMinimumDistance (int s, int e, int mv, bool connected[NUMBERofVERTICES], int vertices[NUMBERofVERTICES][NUMBERofVERTICES], int minimumDistance[NUMBERofVERTICES]);
 
 int *dijkstraDistance(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]){
@@ -24,7 +24,7 @@ int *dijkstraDistance(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]){
 	//    to the tree, and this is indicated by setting CONNECTED[0] = TRUE.
 
 	//    We initialize minimumDistance[I] to the one step distance from node 0 to node I.
- 
+
 	//    Now we search among the unconnected nodes for the node MV whose minimum
 	//    distance is smallest, and connect it to the tree.  For each remaining
 	//    unconnected node I, we check to see whether the distance from 0 to MV
@@ -34,7 +34,7 @@ int *dijkstraDistance(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]){
 	int i, *minimumDistance, inf = 2147483647;
 	int mainDistance, mainIndex;
 	int myFirst = 1, myLast=NUMBERofVERTICES-1, distance, index, myStep;
-	
+
 	// start out with only node 0 connected to the tree
 	connected = new bool[NUMBERofVERTICES];
 	connected[0] = true;
@@ -50,33 +50,33 @@ int *dijkstraDistance(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]){
 		minimumDistance[i] = vertices[0][i];
 	}
 
-		for(myStep=1; myStep<NUMBERofVERTICES; myStep++){
-			mainDistance = inf;
-			mainIndex = -1; 
-			
-			findNearest(myFirst, myLast, minimumDistance, connected, &distance, &index);
-				
-			if(distance < mainDistance){
-				mainDistance = distance;
-				mainIndex = index;
-			}
-			
-			if(mainIndex != - 1){
-				connected[mainIndex] = true;
-				updateMinimumDistance(myFirst, myLast, mainIndex, connected, vertices, minimumDistance);
-			}
+	for(myStep=1; myStep<NUMBERofVERTICES; myStep++){
+		mainDistance = inf;
+		mainIndex = -1; 
+
+		findNearest(myFirst, myLast, minimumDistance, connected, &distance, &index);
+
+		if(distance < mainDistance){
+			mainDistance = distance;
+			mainIndex = index;
 		}
-	
+
+		if(mainIndex != - 1){
+			connected[mainIndex] = true;
+			updateMinimumDistance(myFirst, myLast, mainIndex, connected, vertices, minimumDistance);
+		}
+	}
+
 	delete [] connected;	// free memory
 
 	return minimumDistance;
 }
 
 void findNearest (int first, int last, int minimumDistance[NUMBERofVERTICES], bool connected[NUMBERofVERTICES], int *distance, int *index){
-// output: 
-//	- int *distance, the distance from node 0 to the nearest unconnected node in the range first to last
-//	- int *index, the index of the nearest unconnected node in the range first to last.
-				   
+	// output: 
+	//	- int *distance, the distance from node 0 to the nearest unconnected node in the range first to last
+	//	- int *index, the index of the nearest unconnected node in the range first to last.
+
 	int i, inf=2147483647;
 	*distance = inf;
 	*index = -1;
@@ -89,7 +89,7 @@ void findNearest (int first, int last, int minimumDistance[NUMBERofVERTICES], bo
 	}
 }
 
-void init (int vertices[NUMBERofVERTICES][NUMBERofVERTICES])
+void init (int vertices[NUMBERofVERTICES][NUMBERofVERTICES],int shuf)
 {
 	//    The graph uses 6 nodes, and has the following diagram and
 	//    distance matrix:
@@ -125,14 +125,14 @@ void init (int vertices[NUMBERofVERTICES][NUMBERofVERTICES])
 	}
 
 	// our task
-	vertices[0][1] = vertices[1][0] = 40;
-	vertices[0][2] = vertices[2][0] = 15;
-	vertices[1][2] = vertices[2][1] = 20;
-	vertices[1][3] = vertices[3][1] = 10;
-	vertices[1][4] = vertices[4][1] = 25;
-	vertices[2][3] = vertices[3][2] = 100;
-	vertices[1][5] = vertices[5][1] = 6;
-	vertices[4][5] = vertices[5][4] = 8;
+	vertices[(0+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = vertices[(1+shuf)%NUMBERofVERTICES][(0+shuf)%NUMBERofVERTICES] = 40;
+	vertices[(0+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = vertices[(2+shuf)%NUMBERofVERTICES][(0+shuf)%NUMBERofVERTICES] = 15;
+	vertices[(1+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = vertices[(2+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = 20;
+	vertices[(1+shuf)%NUMBERofVERTICES][(3+shuf)%NUMBERofVERTICES] = vertices[(3+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = 10;
+	vertices[(1+shuf)%NUMBERofVERTICES][(4+shuf)%NUMBERofVERTICES] = vertices[(4+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = 25;
+	vertices[(2+shuf)%NUMBERofVERTICES][(3+shuf)%NUMBERofVERTICES] = vertices[(3+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = 100;
+	vertices[(1+shuf)%NUMBERofVERTICES][(5+shuf)%NUMBERofVERTICES] = vertices[(5+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = 6;
+	vertices[(4+shuf)%NUMBERofVERTICES][(5+shuf)%NUMBERofVERTICES] = vertices[(5+shuf)%NUMBERofVERTICES][(4+shuf)%NUMBERofVERTICES] = 8;
 }
 
 void updateMinimumDistance (int first, int last, int mainIndex, bool connected[NUMBERofVERTICES], int vertices[NUMBERofVERTICES][NUMBERofVERTICES], int minimumDistance[NUMBERofVERTICES]){
@@ -155,37 +155,45 @@ void updateMinimumDistance (int first, int last, int mainIndex, bool connected[N
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int i, j, *minimumDistance, inf = 2147483647;
+	int *minimumDistance, inf = 2147483647;
 	int vertices[NUMBERofVERTICES][NUMBERofVERTICES];
 
-	// inicialization of data
-	init(vertices);	
 
-	// print input
-	cout << "Input matrix of distances\n\n";
+	for (int i = 0; i < NUMBERofVERTICES; i++)
+	{
 
-	for(i=0; i<NUMBERofVERTICES; i++){
-		for(j=0; j<NUMBERofVERTICES; j++){
-			if(vertices[i][j] == inf){
-				cout << setw(6) << "Inf";
+		// inicialization of data
+		init(vertices,-i+NUMBERofVERTICES);	
 
-			}else{
-				cout << setw(6) <<  vertices[i][j];
+		// print input
+		cout << "Input matrix of distances\n\n";
+
+		for(int k=0; k<NUMBERofVERTICES; k++){
+			for(int j=0; j<NUMBERofVERTICES; j++){
+				if(vertices[k][j] == inf){
+					cout << setw(6) << "Inf";
+
+				}else{
+					cout << setw(6) <<  vertices[k][j];
+				}
 			}
+			cout << "\n";
 		}
-		cout << "\n";
+
+
+		minimumDistance = dijkstraDistance(vertices);
+
+		// print the results
+		cout << "\nMinimum distances from node "<< i<<endl;
+
+		for (int j=0; j<NUMBERofVERTICES; j++){
+			cout << setw(4) << j << "  " << setw(4) << minimumDistance[(j-i+NUMBERofVERTICES)%NUMBERofVERTICES] << "\n";
+		}
+
+		delete [] minimumDistance;		// free memory
+
+
 	}
-
-	minimumDistance = dijkstraDistance(vertices);
-
-	// print the results
-	cout << "\nMinimum distances from node 0\n\n";
-
-	for (i=0; i<NUMBERofVERTICES; i++){
-		cout << setw(4) << i << "  " << setw(4) << minimumDistance[i] << "\n";
-	}
-
-	delete [] minimumDistance;		// free memory
 
 	system ("pause");
 	return 0;
