fab1f90b1f0adac997ecf59e981e44b4217f5545
Marek <bartimar6@gmail.com>
2014-03-12 22:11:45 +0100

    switch power

--- PAP/PAP.cpp
+++ PAP/PAP.cpp
@@ -13,9 +13,9 @@ using namespace std;
 const int inf = INT_MAX;
 
 int *dijkstraDistance (int vertices[NUMBERofVERTICES][NUMBERofVERTICES]);
-void findNearest (int s, int e, int minimumDistance[NUMBERofVERTICES], bool connected[NUMBERofVERTICES], int& d, int& v);
+void findNearest (int minimumDistance[NUMBERofVERTICES], bool connected[NUMBERofVERTICES], int& d, int& v);
 void init (int vertices[NUMBERofVERTICES][NUMBERofVERTICES],int=0, int=1);
-void updateMinimumDistance (int s, int e, int mv, bool connected[NUMBERofVERTICES], int vertices[NUMBERofVERTICES][NUMBERofVERTICES], int minimumDistance[NUMBERofVERTICES]);
+void updateMinimumDistance (int mv, bool connected[NUMBERofVERTICES], int vertices[NUMBERofVERTICES][NUMBERofVERTICES], int minimumDistance[NUMBERofVERTICES]);
 
 void floydWarshall(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]){
 	int i, j, k;
@@ -57,39 +57,30 @@ void printVertices(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]) {
 
 int *dijkstraDistance(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]){
 	bool *connected;
-	int i, *minimumDistance;
-	int mainDistance, mainIndex;
-	int myFirst = 1, myLast=NUMBERofVERTICES-1, distance, index, myStep;
+	int *minimumDistance;
+	int myFirst = 1, myLast=NUMBERofVERTICES-1, distance, index;
 
 	// start out with only node 0 connected to the tree
 	connected = new bool[NUMBERofVERTICES];
 	connected[0] = true;
 
-	for(i=1; i<NUMBERofVERTICES; i++){
+	for(int i=1; i<NUMBERofVERTICES; i++)
 		connected[i] = false;
-	}
 
 	// initialize the minimum distance to the one-step distance
 	minimumDistance = new int[NUMBERofVERTICES];
 
-	for(i=0; i<NUMBERofVERTICES; i++){
+	for(int i=0; i<NUMBERofVERTICES; i++)
 		minimumDistance[i] = vertices[0][i];
-	}
 
-	for(myStep=1; myStep<NUMBERofVERTICES; myStep++){
-		mainDistance = inf;
-		mainIndex = -1; 
 
-		findNearest(myFirst, myLast, minimumDistance, connected, distance, index);
+	for(int step=1; step<NUMBERofVERTICES; step++){
+		
+		findNearest(minimumDistance, connected, distance, index);
 
-		if(distance < mainDistance){
-			mainDistance = distance;
-			mainIndex = index;
-		}
-
-		if(mainIndex != - 1){
-			connected[mainIndex] = true;
-			updateMinimumDistance(myFirst, myLast, mainIndex, connected, vertices, minimumDistance);
+		if(distance < inf){
+			connected[index] = true;
+			updateMinimumDistance(index, connected, vertices, minimumDistance);
 		}
 	}
 
@@ -98,7 +89,7 @@ int *dijkstraDistance(int vertices[NUMBERofVERTICES][NUMBERofVERTICES]){
 	return minimumDistance;
 }
 
-void findNearest(int first, int last, int minimumDistance[NUMBERofVERTICES], bool connected[NUMBERofVERTICES], int& distance, int& index){
+void findNearest(int minimumDistance[NUMBERofVERTICES], bool connected[NUMBERofVERTICES], int& distance, int& index){
 	// output: 
 	//	- int distance, the distance from node 0 to the nearest unconnected node in the range first to last
 	//	- int index, the index of the nearest unconnected node in the range first to last.
@@ -106,7 +97,7 @@ void findNearest(int first, int last, int minimumDistance[NUMBERofVERTICES], boo
 	distance = inf;
 	index = -1;
 
-	for(int i=first; i<=last; i++){
+	for(int i=1; i<NUMBERofVERTICES; i++){
 		if(!connected[i] && minimumDistance[i] < distance){
 			distance = minimumDistance[i];
 			index = i;
@@ -115,18 +106,17 @@ void findNearest(int first, int last, int minimumDistance[NUMBERofVERTICES], boo
 }
 
 void init(int vertices[NUMBERofVERTICES][NUMBERofVERTICES],int shuf, int example){
+	
 	for(int i=0; i<NUMBERofVERTICES; i++){
 		for(int j=0; j<NUMBERofVERTICES; j++){
-			if(i==j){					// the same vertices, distance = 0			
-				vertices[i][i] = 0;
-
-			}else{
-				vertices[i][j] = inf;	// inicialization of all the other vertices to inf
-			}
+			if(i==j) vertices[i][i] = 0;
+			else vertices[i][j] = inf;	// inicialization of all the other vertices to inf
 		}
 	}
 
-	if(example == 1){
+	switch(example){
+
+	case 1:
 		vertices[(0+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = vertices[(1+shuf)%NUMBERofVERTICES][(0+shuf)%NUMBERofVERTICES] = 40;
 		vertices[(0+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = vertices[(2+shuf)%NUMBERofVERTICES][(0+shuf)%NUMBERofVERTICES] = 15;
 		vertices[(1+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = vertices[(2+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = 20;
@@ -135,7 +125,8 @@ void init(int vertices[NUMBERofVERTICES][NUMBERofVERTICES],int shuf, int example
 		vertices[(2+shuf)%NUMBERofVERTICES][(3+shuf)%NUMBERofVERTICES] = vertices[(3+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = 100;
 		vertices[(1+shuf)%NUMBERofVERTICES][(5+shuf)%NUMBERofVERTICES] = vertices[(5+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = 6;
 		vertices[(4+shuf)%NUMBERofVERTICES][(5+shuf)%NUMBERofVERTICES] = vertices[(5+shuf)%NUMBERofVERTICES][(4+shuf)%NUMBERofVERTICES] = 8;
-	}else if(example == 2){
+		break;
+	case 2:
 		vertices[(0+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = 8;
 		vertices[(0+shuf)%NUMBERofVERTICES][(3+shuf)%NUMBERofVERTICES] = 1;
 		vertices[(0+shuf)%NUMBERofVERTICES][(4+shuf)%NUMBERofVERTICES] = 2;
@@ -150,19 +141,15 @@ void init(int vertices[NUMBERofVERTICES][NUMBERofVERTICES],int shuf, int example
 		vertices[(5+shuf)%NUMBERofVERTICES][(1+shuf)%NUMBERofVERTICES] = 8;
 		vertices[(5+shuf)%NUMBERofVERTICES][(2+shuf)%NUMBERofVERTICES] = 3;
 		vertices[(5+shuf)%NUMBERofVERTICES][(6+shuf)%NUMBERofVERTICES] = 2;
+		break;
 	}
 }
 
-void updateMinimumDistance(int first, int last, int mainIndex, bool connected[NUMBERofVERTICES], int vertices[NUMBERofVERTICES][NUMBERofVERTICES], int minimumDistance[NUMBERofVERTICES]){
-	for(int i=first; i <= last; i++){
-		if(!connected[i]){
-
-			if(vertices[mainIndex][i] < inf){
-
-				if(minimumDistance[mainIndex] + vertices[mainIndex][i] < minimumDistance[i]){
+void updateMinimumDistance(int mainIndex, bool connected[NUMBERofVERTICES], int vertices[NUMBERofVERTICES][NUMBERofVERTICES], int minimumDistance[NUMBERofVERTICES]){
+	for(int i=1;i< NUMBERofVERTICES; i++){
+		if(connected[i] || vertices[mainIndex][i] == inf) continue;
+		if(minimumDistance[mainIndex] + vertices[mainIndex][i] < minimumDistance[i]){
 					minimumDistance[i] = minimumDistance[mainIndex] + vertices[mainIndex][i];
-				}
-			}
 		}
 	}
 }
